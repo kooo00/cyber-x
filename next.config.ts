367dd586ecc1251a777527bbd4cba741863fead7
koo00 <x1108423@gmail.com>
2026-07-01 14:53:28 +0900

    他の端末でもログインができるようにした

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.56.1"],
+  allowedDevOrigins: ["172.31.2.112"],
 };
 
 export default nextConfig;
